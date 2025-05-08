a7c8980139e93379ba632e00228e8ce7c64e38ae
devmdfaiz <inoogobyfaizan@gmail.com>
2025-05-09 00:04:08 +0530

    Backend build command added

--- apps/backend/dist/lib/database/db.js
+++
@@ -1,5 +0,0 @@
-import { Database, allSchemas } from "@smm-guru/database";
-import { DATABASE_URL } from "../env.js";
-const database = new Database(DATABASE_URL, allSchemas);
-export const db = database.db;
-export const schema = database.schema;
--- packages/utils/build/constants/crypto.constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IV_LENGTH = exports.ALGORITHM = void 0;
-exports.ALGORITHM = "aes-256-cbc";
-exports.IV_LENGTH = 16; // For AES, this is always 16
-//# sourceMappingURL=crypto.constants.js.map
